da5a20dd18f823fe7a67de60185f2c35d301da2a
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-09-10 15:33:34 +0200

    text-left

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="grid-cols-1 md:flex bg-blue-800 opacity-100">
+    <div className="grid-cols-1 md:flex bg-blue-900 ">
         <div className="container mx-auto md:flex md:gap-40">
-            <div className="px-10 md:py-10 text-center">
+            <div className="px-10 md:py-10 text-center md:text-left">
                 <h1 className="text-green-500 py-5">SKILLS DEVELOPMENT</h1>
                 <ul className="text-white text-sm">
                     <li>Mollo Animation Academy and Studio</li>
@@ -14,7 +14,7 @@ const Footer = () => {
                     <li>Internet of Things</li>
                 </ul>
             </div>
-            <div className="px-10 md:py-10 text-center">
+            <div className="px-10 md:py-10 text-center md:text-left">
                 <h1 className="text-cyan-400 py-5">INCUBATION & ACCELERATION</h1>
                 <ul className="text-white text-sm">
                     <li>Design Thinking Workshop</li>
@@ -25,7 +25,7 @@ const Footer = () => {
                     <li>Makerspace</li>
                 </ul>
             </div>
-            <div className="px-10 md:py-10 text-center">
+            <div className="px-10 md:py-10 text-center md:text-left">
                 <h1 className="text-orange-600 py-5">MARKET ACCESS</h1>
                 <ul className="text-white text-sm pb-10">
                     <li>Fak'ugesi Festival</li>
