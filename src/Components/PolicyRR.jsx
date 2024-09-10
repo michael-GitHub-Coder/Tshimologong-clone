00925b96220501ed0e15c1f01bc5e03cfca94a8b
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-09-10 15:16:28 +0200

    Games

--- src/Components/PolicyRR.jsx
+++ src/Components/PolicyRR.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const PolicyRR = () => {
   return (
-    <div className="bg-gray-100 grid-cols-1 md:flex justify-between px-7 py-1 text-[12px] text-gray-500">
+    <div className="bg-gray-100 grid-cols-1 text-center md:flex justify-between px-7 py-1 text-[12px] text-gray-500">
         <p>©2021 Tshimologong. All rights reserved.</p>
-        <div className="flex gap-3">
+        <div className="text-center md:flex gap-3">
             <a href="#">Privacy Policy</a>
             <a href="#">PAIA</a>
             <a href="#">Terms and Conditions</a>
