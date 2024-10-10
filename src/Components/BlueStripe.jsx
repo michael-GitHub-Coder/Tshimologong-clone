7f279f30a88470d76948d8560073276e53f3d8ec
Michael <motlokwa.thulare@gmail.com>
2024-10-10 15:35:53 +0200

    w

--- src/Components/BlueStripe.jsx
+++ src/Components/BlueStripe.jsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
 const BlueStripe = () => {
   return (
     <div className="hidden md:flex md:justify-end md:text-white md:gap-4 md:mr-5 md:py-2">
-        <FaFacebook className="cursor-pointer"/> <FaTwitter /> <FaYoutube /> <FaLinkedin />
+        <FaFacebook className="cursor-pointer"/> <FaTwitter className="cursor-pointer"/> <FaYoutube /> <FaLinkedin />
     </div>
   )
 }
